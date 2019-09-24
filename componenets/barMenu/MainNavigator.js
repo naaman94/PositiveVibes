2eb8754699fc30bbcf7795ca934c1f11e4eebd63
AsmaAlfauri <51743787+AsmaAlfauri@users.noreply.github.com>
2019-09-24 16:23:30 +0300

    add sittings files

--- componenets/barMenu/MainNavigator.js
+++ componenets/barMenu/MainNavigator.js
@@ -3,11 +3,15 @@ import {createStackNavigator} from 'react-navigation-stack';
 import Home from './Home';
 import Settings from './Settings';
 import Catigories from './Catigories';
+import MyCatigories from './MyCatigories';
+import Notifications from './Notifications';
 
 const MainNavigator = createStackNavigator({
   Home: {screen: Home},
   Settings: {screen: Settings},
   Catigories: {screen: Catigories},
+  MyCatigories: {screen: MyCatigories},
+  Notifications: {screen: Notifications},
 });
 
 export default createAppContainer(MainNavigator);
